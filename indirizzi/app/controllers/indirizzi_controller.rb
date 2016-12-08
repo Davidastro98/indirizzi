253334aaee05501ea51feb0e8df296ea08719a82
vannigio <giovanni.vanini@gmail.com>
2016-12-09 00:30:46 +0100

    sistemata la applicazione base

--- indirizzi/app/controllers/indirizzi_controller.rb
+++ indirizzi/app/controllers/indirizzi_controller.rb
@@ -1,8 +1,23 @@
 class IndirizziController < ApplicationController
 	def home
+		@posizioni = Position.all
 	end
+
 	def new
 	end
+
 	def create
-	end 
+		@posizione = Position.new(parametri_posizione)
+
+		if @posizione.save
+			redirect_to indirizzi_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def parametri_posizione
+		params.require(:posizione).permit(:name,:address)
+	end
 end
