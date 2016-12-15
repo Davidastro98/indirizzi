359b69161a6efc5d667489ffd980a0b6521a5f35
vannigio <giovanni.vanini@gmail.com>
2016-12-16 00:00:54 +0100

    Aggiunto autocompletamento indirizzi

--- indirizzi/db/schema.rb
+++ indirizzi/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208221204) do
+ActiveRecord::Schema.define(version: 20161211225117) do
 
   create_table "positions", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
