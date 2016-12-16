e78969b6f95b0587f488d9bc858741fc625829ef
Luca Rillosi <luca.rillosi@gmail.com>
2016-12-16 15:25:40 +0100

    Sistemate le view e il db

--- indirizzi/app/controllers/indirizzi_controller.rb
+++ indirizzi/app/controllers/indirizzi_controller.rb
@@ -1,8 +1,16 @@
 class IndirizziController < ApplicationController
 	def home
+		@posizioni = Position.all
 	end
 	def new
 	end
 	def create
+		@posizione = Position.new(parametri_posizione)
+		if @posizione.save
+			redirect_to indirizzi_path
+		end
 	end 
+	def parametri_posizione
+		params.require(:posizione).permit(:name,:address)
+	end
 end
---
+++ indirizzi/db/migrate/20161216082919_addname_topositions.rb
@@ -0,0 +1,4 @@
+class AddnameTopositions < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
